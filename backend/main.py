1a5e5d9691f7ad7695eb8c0754006e664f7c7d36
Aathif Zahir <aathifzahir12@gmail.com>
2025-04-15 01:06:44 +0530

    feat: backend ai integrate

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,9 @@ from fastapi.responses import JSONResponse
 from utils.file_handler import handle_uploaded_zip
 from utils.repo_handler import handle_github_clone
 from schemas.schemas import RepoCloneRequest
+from utils.readme_generator import generate_readme_summary
 
+from utils.ai_handler import generate_readme
 
 app = FastAPI()
 
@@ -20,3 +22,10 @@ async def upload_zip(file: UploadFile = File(...)):
 async def repo_clone(body: RepoCloneRequest):
     result = handle_github_clone(body.url)
     return JSONResponse(content=result)
+    
+
+@app.post("/generate-readme")
+async def generate_readme_endpoint(code_summary: str):
+    readme = generate_readme(code_summary)
+    return JSONResponse(content={"readme": readme})
+
